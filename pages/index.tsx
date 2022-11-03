51bdab2eff979c33c6c11c926d2bd6e33cf89576
lucas guillemette <36134032+lgilmet@users.noreply.github.com>
2022-11-02 20:01:25 -0400

    Fix center align on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,13 @@ import Head from "next/head";
 
 export default function Home() {
     return (
-        <div className=" absolute top-0  min-h-screen w-screen flex flex-col justify-center">
+        <div className="absolute top-0 left-0 min-h-screen w-screen flex flex-col justify-center">
             <Head>
                 <title>Talent Agency</title>
                 <meta name="description" content="NextJs with Sanity" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="w-[50%] text-center mx-auto">Home</div>
+            <div className="text-center mx-auto">Home</div>
         </div>
     );
 }
